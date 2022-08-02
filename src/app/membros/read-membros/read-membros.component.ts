cda6df82a12d75a0a119daf49e14d9be772bef54
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-02 09:47:29 -0300

    function delete member

--- src/app/membros/read-membros/read-membros.component.ts
+++ src/app/membros/read-membros/read-membros.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { CreateMembrosService } from './../../components/membros/create-membros.service';
 import { Membro } from './../../components/membros/membro.model';
 import { Component, OnInit } from '@angular/core';
@@ -10,8 +11,11 @@ import { Component, OnInit } from '@angular/core';
 export class ReadMembrosComponent implements OnInit {
 
   membros: Membro[] = []
+  url = "https://api-go-projects.herokuapp.com/membros/"
+
 
   constructor(
+    private http: HttpClient,
     private membrosService: CreateMembrosService
   ) { }
 
@@ -20,6 +24,32 @@ export class ReadMembrosComponent implements OnInit {
       this.membros = membros
       console.log(membros)
     } )
+  }
+
+  listaMembros() {
+    this.http.get(this.url).subscribe( resultado => 
+      console.log(resultado)
+    )
+  }
+
+  listaMembro () {
+    this.http.get(`${this.url}1`).subscribe( resultado =>
+      console.log(resultado)
+    )
+  }
+
+  adicionaMembro () {
+
+  }
+
+  atualizaMembro () {
+
+  }
+
+  deletaMembro (id: any) {
+    this.http.delete(`${this.url}${id}`).subscribe( resultado =>
+      console.log(resultado)
+    )
   }
 
 }
