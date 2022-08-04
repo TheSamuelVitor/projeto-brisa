f6a285539f98f196ce8e3b069eebc2c92461da7f
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-04 11:43:25 -0300

    update dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { HttpClient } from '@angular/common/http';
-import { CountService } from './../../components/count.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,23 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   
   constructor( 
-    // private countService: CountService,
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
-  //   this.countService.countEquipes()
-  //   this.countService.countMembros()
-  //   this.countService.countProjetos()
-  //   this.countService.countTarefas()
-  }
-
-  qtdEquipes() {
-    this.http.get('https://api-go-projects.herokuapp.com/membros/count').subscribe(
-      resultado => {
-        console.log(resultado)
-      }
-    )
   }
 
 }
