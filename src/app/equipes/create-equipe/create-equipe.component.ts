d584e488b6cca9e9e3040d76aab0b08c771b4a5d
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-05 11:39:29 -0300

    add required function in form and input

--- src/app/equipes/create-equipe/create-equipe.component.ts
+++ src/app/equipes/create-equipe/create-equipe.component.ts
@@ -1,39 +1,54 @@
-import { CreateEquipesService } from './../../components/equipes/create-equipes.service';
-import { Equipe } from './../../components/equipes/equipe.model';
-import { AvisoService } from './../../components/aviso.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { CreateEquipesService } from "./../../components/equipes/create-equipes.service";
+import { Equipe } from "./../../components/equipes/equipe.model";
+import { AvisoService } from "./../../components/aviso.service";
+
+import { FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-create-equipe',
-  templateUrl: './create-equipe.component.html',
-  styleUrls: ['./create-equipe.component.css']
+  selector: "app-create-equipe",
+  templateUrl: "./create-equipe.component.html",
+  styleUrls: ["./create-equipe.component.css"],
 })
 export class CreateEquipeComponent implements OnInit {
+  formulario!: FormGroup;
 
   equipe: Equipe = {
-    nome_equipe: ""
-  }
+    nome_equipe: "",
+  };
 
-  constructor(private router: Router,
+  constructor(
+    private createEquipeService: CreateEquipesService,
+    private formBuilder: FormBuilder,
     private aviso: AvisoService,
-    private createEquipeService: CreateEquipesService
-    ) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nome: [null, [Validators.required]]
+    })
   }
 
   goBack(): void {
-    this.router.navigate(['equipes'])
+    this.router.navigate(["equipes"]);
   }
 
   salvar(): void {
-    this.createEquipeService.create(this.equipe).subscribe( 
-      () => {
-        this.goBack()
-        this.aviso.showMsg({msg: 'Equipe criada com sucesso'})
-      }
-    )
+    this.createEquipeService.create(this.equipe).subscribe(() => {
+      this.aviso.showMsg({ msg: "Equipe criada com sucesso" });
+      this.goBack();
+    });
   }
 
+  limpar(): void {
+    this.formulario.reset()
+  }
+
+  onSubmit(): void {
+    console.log(this.formulario.controls)
+    this.formulario.reset()
+  }
 }
--- src/app/membros/create-membros/create-membros.component.ts
+++ src/app/membros/create-membros/create-membros.component.ts
@@ -1,48 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
-import { Observable } from 'rxjs';
+import { CreateEquipesService } from "./../../components/equipes/create-equipes.service";
+import { CreateMembrosService } from "./../../components/membros/create-membros.service";
+import { Equipe } from "./../../components/equipes/equipe.model";
+import { Membro } from "./../../components/membros/membro.model";
+import { AvisoService } from "./../../components/aviso.service";
 
-import { CreateEquipesService } from './../../components/equipes/create-equipes.service';
-import { CreateMembrosService } from './../../components/membros/create-membros.service';
-import { Equipe } from './../../components/equipes/equipe.model';
-import { Membro } from './../../components/membros/membro.model';
-import { AvisoService } from './../../components/aviso.service';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-create-membros',
-  templateUrl: './create-membros.component.html',
-  styleUrls: ['./create-membros.component.css']
+  selector: "app-create-membros",
+  templateUrl: "./create-membros.component.html",
+  styleUrls: ["./create-membros.component.css"],
 })
 export class CreateMembrosComponent implements OnInit {
+
+  formulario!: FormGroup;
 
   membro: Membro = {
     nome_membro: "",
     funcao: "",
-    id_equipe: 0
-  }
-  
-  equipes: Observable<Equipe[]> | undefined
+    id_equipe: 0,
+  };
+
+  equipes: Observable<Equipe[]> | undefined;
 
   constructor(
     private createMembroService: CreateMembrosService,
     private equipesService: CreateEquipesService,
+    private formBuilder: FormBuilder,
     private avisoMsg: AvisoService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.equipes = this.equipesService.read();
+
+    this.formulario = this.formBuilder.group({
+      nome: [null, [Validators.required]]
+    });
   }
 
   goBack(): void {
-    this.router.navigate(['membros'])
+    this.router.navigate(["membros"]);
   }
 
   salvar(): void {
-    this.createMembroService.create(this.membro).subscribe( () => {
-      this.goBack()
-      this.avisoMsg.showMsg({msg: ' Membro criado com sucesso '})
-    })
+    this.createMembroService.create(this.membro).subscribe(() => {
+      this.goBack();
+      this.avisoMsg.showMsg({ msg: " Membro criado com sucesso " });
+    });
   }
 
+  onSubmit(): void {
+
+  }
 }
