6c9b8443b6caeff9fb66034ba6757f235f3441a9
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-10 10:17:26 -0300

    update equipe

--- src/app/components/equipes/equipes.service.ts
+++ src/app/components/equipes/equipes.service.ts
@@ -1,32 +1,31 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Equipe } from './equipe.model';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Equipe } from "./equipe.model";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CreateEquipesService {
+  baseUrl = "https://api-go-projects.herokuapp.com/equipes/";
 
-  baseUrl = 'https://api-go-projects.herokuapp.com/equipes/'
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   create(equipe: Equipe): Observable<Equipe> {
-    return this.http.post<Equipe>(this.baseUrl, equipe)
+    return this.http.post<Equipe>(this.baseUrl, equipe);
   }
 
   read(): Observable<Equipe[]> {
-    return this.http.get<Equipe[]>(this.baseUrl)
+    return this.http.get<Equipe[]>(this.baseUrl);
   }
 
-  getById(id: string): Observable<Equipe> {
-    const url= `${this.baseUrl}/${id}`
-    return this.http.get<Equipe>(url)
+  getById({ id }: { id: string }): Observable<Equipe> {
+    const url = `${this.baseUrl}${id}`;
+    return this.http.get<Equipe>(url);
   }
 
   update(equipe: Equipe): Observable<Equipe> {
-    const url= `${this.baseUrl}/${equipe.id_equipe}`
-    return this.http.put<Equipe>(url,equipe)
+    const url = `${this.baseUrl}/${equipe.id_equipe}`;
+    return this.http.put<Equipe>(url, equipe);
   }
 }
--- src/app/equipes/update-equipe/update-equipe.component.ts
+++ src/app/equipes/update-equipe/update-equipe.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { Equipe } from "src/app/components/equipes/equipe.model";
+import { CreateEquipesService } from "src/app/components/equipes/equipes.service";
 
 @Component({
-  selector: 'app-update-equipe',
-  templateUrl: './update-equipe.component.html',
-  styleUrls: ['./update-equipe.component.css']
+  selector: "app-update-equipe",
+  templateUrl: "./update-equipe.component.html",
+  styleUrls: ["./update-equipe.component.css"],
 })
 export class UpdateEquipeComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private equipeService: CreateEquipesService,
+    private route: ActivatedRoute,
+    private form: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.equipeService.getById({ id: `${id}` }).subscribe((equipe) => {
+      this.equipe = equipe;
+      console.log(equipe);
+    });
   }
 
+  formulario = this.form.group({
+    nome: [null, Validators.required],
+  });
+
+  equipe: Equipe | undefined;
+
+  onSubmit(): void {}
+
+  goBack(): void {
+    this.router.navigate(["/equipes"]);
+  }
+
+  limpar(): void {
+    this.formulario.reset();
+  }
 }
