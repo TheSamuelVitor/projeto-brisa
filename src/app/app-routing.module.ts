2d1253b4bf3998ec149335506cc5838d6319080d
thesamuelvitor <thesamuelvitor.py@gmail.com>
2022-09-06 11:23:04 -0300

    :ok_hand: feat: codigo reestruturado para facilidade de leitura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,106 +1,106 @@
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
 
-import { AuthGuard } from './guards/auth-guard.guard';
-import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from "./guards/auth-guard.guard";
+import { LoginComponent } from "./pages/login/login.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
-import { EquipeComponent } from './pages/equipes/equipe/equipe.component';
-import { PaginaEquipeComponent } from './pages/equipes/pagina-equipe/pagina-equipe.component';
-import { CreateEquipeComponent } from './pages/equipes/create-equipe/create-equipe.component';
-import { UpdateEquipeComponent } from './pages/equipes/update-equipe/update-equipe.component';
+import { EquipeComponent } from "./pages/equipes/equipe/equipe.component";
+import { PaginaEquipeComponent } from "./pages/equipes/pagina-equipe/pagina-equipe.component";
+import { CreateEquipeComponent } from "./pages/equipes/create-equipe/create-equipe.component";
+import { UpdateEquipeComponent } from "./pages/equipes/update-equipe/update-equipe.component";
 
-import { MembroComponent } from './pages/membros/membro/membro.component';
-import { CreateMembrosComponent } from './pages/membros/create-membros/create-membros.component';
-import { UpdateMembroComponent } from './pages/membros/update-membro/update-membro.component';
+import { MembroComponent } from "./pages/membros/membro/membro.component";
+import { CreateMembrosComponent } from "./pages/membros/create-membros/create-membros.component";
+import { UpdateMembroComponent } from "./pages/membros/update-membro/update-membro.component";
 
-import { CreateProjetosComponent } from './pages/projetos/create-projetos/create-projetos.component';
-import { UpdateProjetoComponent } from './pages/projetos/update-projeto/update-projeto.component';
-import { ProjetosComponent } from 'src/app/pages/projetos/projetos/projetos.component';
+import { CreateProjetosComponent } from "./pages/projetos/create-projetos/create-projetos.component";
+import { UpdateProjetoComponent } from "./pages/projetos/update-projeto/update-projeto.component";
+import { ProjetosComponent } from "src/app/pages/projetos/projetos/projetos.component";
 
-import { TarefasComponent } from 'src/app/pages/tarefas/tarefas/tarefas.component';
-import { CreateTarefasComponent } from './pages/tarefas/create-tarefas/create-tarefas.component';
-import { UpdateTarefaComponent } from './pages/tarefas/update-tarefa/update-tarefa.component';
+import { TarefasComponent } from "src/app/pages/tarefas/tarefas/tarefas.component";
+import { CreateTarefasComponent } from "./pages/tarefas/create-tarefas/create-tarefas.component";
+import { UpdateTarefaComponent } from "./pages/tarefas/update-tarefa/update-tarefa.component";
 
 const routes: Routes = [
   {
     path: "login",
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: "",
     component: DashboardComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "equipes",
     component: EquipeComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "equipes/dados/:id",
     component: PaginaEquipeComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "equipes/create",
     component: CreateEquipeComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "equipes/update/:id",
     component: UpdateEquipeComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "membros",
     component: MembroComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "membros/create",
     component: CreateMembrosComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "membros/update/:id",
     component: UpdateMembroComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
-    path:"projetos",
+    path: "projetos",
     component: ProjetosComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "projetos/create",
     component: CreateProjetosComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
-    path:"projetos/update/:id",
+    path: "projetos/update/:id",
     component: UpdateProjetoComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "tarefas",
     component: TarefasComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: "tarefas/create",
     component: CreateTarefasComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
-    path:"tarefas/update/:id",
+    path: "tarefas/update/:id",
     component: UpdateTarefaComponent,
-    canActivate: [AuthGuard]
-  }
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
